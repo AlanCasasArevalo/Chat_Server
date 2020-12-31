2472cc70261944b0a7d41eb9da8d691638c83171
Alan Casas <alancasasarevalo@gmail.com>
2020-12-31 11:43:19 +0100

    [FEATURE][AUTH] * Fields validator was implemented

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,6 @@
 const {validationResult} = require("express-validator");
 const { response } = require('express')
 const post = async (req, res = response) => {
-
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        return  res.status(400).json({
-            result: false,
-            errors: errors.mapped()
-        })
-    }
-
     res.status(200).json({
         result: true,
         message: 'Post message'
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,13 +1,16 @@
 const { Router } = require('express')
 const {check} = require("express-validator");
 const authController = require('../controllers/authController')
+const {fieldsValidator} = require("../middlewares/fields_validator");
 
 const router = Router();
 router.post('/new',
     [
         check('name', 'name is required').not().isEmpty(),
         check('email', 'email is required').not().isEmpty(),
+        check('email', 'email is wrong formatted').isEmail(),
         check('password', 'password is required').not().isEmpty(),
+        fieldsValidator
     ],
     authController.post)
 module.exports = router
