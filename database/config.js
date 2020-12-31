322d884b157dbc9996b7c378a48b9ddc01fbcf8a
Alan Casas <alancasasarevalo@gmail.com>
2020-12-31 10:34:00 +0100

    [FEATURE][SETUP] * Mongoose connection was implemented to Atlas

--- database/config.js
+++ database/config.js
@@ -1,9 +1,14 @@
-const moongoose = require('mongoose')
+const mongoose = require('mongoose')
 
 const dbConnection = async () => {
     const errorMessage = `Error al conectarse a mongo - hablar con el administrador`
     try {
-        console.log(`Inicio de configuracion`)
+        await mongoose.connect(process.env.DB_CNN, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true
+        });
+        console.log(`Base de datos de Mongo Conectada a Atlas`)
     } catch (error) {
         console.log(errorMessage, error)
         throw new Error(errorMessage)
