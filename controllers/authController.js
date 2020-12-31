62dcce017888e309729db636dc6af53133760be4
Alan Casas <alancasasarevalo@gmail.com>
2020-12-31 13:08:10 +0100

    [FEATURE][POST USER] * User model was modified to return different object instead all database object * Some validations were added to post user

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,21 +3,45 @@ const User = require('../models/user')
 
 const post = async (req, res = response) => {
 
+    const { email } = req.body
 
-    const user = new User(req.body)
-    const result = await user.save()
+    try {
+        // Se puede hacer asi, o bien como esta en el catch.
+        // const emailExists = await User.findOne({email})
+        //
+        // if (!emailExists || typeof emailExists === 'undefined') {
+        //     res.status(400).json({
+        //         result: false,
+        //         message: 'Email is already in data base'
+        //     })
+        // }
 
-    if (result.errors || result.errors !== 'undefined') {
-        res.status(500).json({
+        const user = new User(req.body)
+        const result = await user.save()
+        if (result.errors || typeof result.errors !== 'undefined') {
+            res.status(500).json({
+                result: false,
+                message: 'Error while insert on database'
+            })
+        }
+        res.status(201).json({
+            result: true,
+            user
+        })
+    } catch (error) {
+        if (error.code === 11000) {
+            return res.status(400).json({
+                result: false,
+                message: 'Credentials are wrong'
+            })
+        }
+        return res.status(500).json({
             result: false,
-            message: 'Error while insert on database'
+            message: `Error while insert on database ${error}`
         })
     }
 
-    res.status(201).json({
-        result: true,
-        message: 'User was created'
-    })
+
 }
 
 module.exports = {
